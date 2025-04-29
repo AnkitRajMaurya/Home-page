12a0d3a84b5f9459d5742cd41290e2034b61cdfe
Ankit Raj <ankit5242raj@gmail.com>
2025-04-29 10:31:39 +0530

    Delete quotes.js

--- quotes.js
+++
@@ -1,8 +0,0 @@
-const quotes = [
-  "Code is like humor. When you have to explain it, it’s bad.",
-  "Experience is the name everyone gives to their mistakes.",
-  "In order to be irreplaceable, one must always be different.",
-  "First, solve the problem. Then, write the code."
-];
-
-document.getElementById('quoteText').innerText = quotes[Math.floor(Math.random() * quotes.length)];
